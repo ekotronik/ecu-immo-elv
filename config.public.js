34250fb1681d6432f3352b46a8f8a2c3054604ad
ekotronik <ekotronik2@wp.pl>
2026-02-22 19:36:31 +0100

    Update config.public.js

--- config.public.js
+++ config.public.js
@@ -2,9 +2,8 @@
 // UZUPEŁNIJ DANYMI Z SUPABASE (Project URL + anon public key)
 // oraz Cloudinary (Cloud name + upload preset)
 window.EKO_CONFIG = {
-  SUPABASE_URL: https://zcjqjlubarucrjmulekg.supabase.co
-  SUPABASE_ANON_KEY:sb_publishable_0Q3X9ggMMA2WP-X2Zbg9jg_aDEcswea
-
+  SUPABASE_URL: "https://zcjqjlubarucrjmulekg.supabase.co" ,
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjanFqbHViYXJ1Y3JqbXVsZWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDA5MDAsImV4cCI6MjA4NzI3NjkwMH0.fzowgNVQnulSUb-dzMVA1FlpfTm8RMqnxEWvCZPEADA" ,
   // używane tylko przez /admin.html do uploadu zdjęć
   CLOUDINARY_CLOUD_NAME: "YOUR_CLOUD_NAME",
   CLOUDINARY_UPLOAD_PRESET: "YOUR_UNSIGNED_UPLOAD_PRESET"
